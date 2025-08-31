b6a9832b1898d8a90a152d8099e1c69e2ff0d23e
Ryan <Ryan@localhost.com>
2025-08-31 09:16:42 +0700

    The conversation_id can now be returned with the user detail informations

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,8 +2,11 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { withAuth, AuthenticatedRequest } from "@/lib/authMiddleware";
 
-export const GET = withAuth(async () => {
+export const GET = withAuth(async (request: AuthenticatedRequest) => {
   try {
+    const currentUser = request.user;
+
+    // 1. Get the list of users from table
     const users = await prisma.users.findMany({
       select: {
         id: true,
@@ -11,13 +14,44 @@ export const GET = withAuth(async () => {
         email: true,
         created_at: true,
         updated_at: true
+      },
+      where: {
+        id: {
+          not: parseInt(currentUser!.id)
+        }
       }
     });
+
+    // 2. Get conversations with other members
+    const conversations = await prisma.conversation_members.findMany({
+      where: {
+        conversations: {
+          conversation_members: {
+            some: {
+              user_id: parseInt(currentUser!.id)
+            }
+          }
+        }
+      },
+      select: {
+        conversation_id: true,
+        user_id: true
+      }
+    });
+    console.log("==>", conversations)
     
-    const serializedUsers = users.map(user => ({
-      ...user,
-      id: user.id.toString()
-    }));
+    // 3. Match each user with their conversation ID with current user
+    const serializedUsers = users.map(user => {
+      const userConversation = conversations.find(conv => 
+        conv.user_id.toString() === user.id.toString() && conv.user_id.toString() !== currentUser!.id
+      );
+      
+      return {
+        ...user,
+        id: user.id.toString(),
+        conversation_id: userConversation ? userConversation.conversation_id.toString() : null
+      };
+    });
     return NextResponse.json(serializedUsers);
   } catch (error) {
     return NextResponse.json({ error: `${error}` }, { status: 500 });
